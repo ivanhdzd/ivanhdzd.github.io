8c4c089f0cd36e584243642228269bca02ab46cf
IvanHdzD <ivanhdzd@hotmail.com>
2018-12-01 00:12:27 -0600

    Adds functionality to 404 page not found.

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-page-not-found',
@@ -7,12 +8,22 @@ import { Title } from '@angular/platform-browser';
 	styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
+	/** Seconds count */
+	public seconds: number = 5;
 
-	constructor(title: Title) {
+	constructor(title: Title, private router: Router) {
 		title.setTitle('IvánHdzD - 404 página no encontrada!');
 	}
 
+	/**
+	 * Start seconds decrement to redirect to root route.
+	 */
 	public ngOnInit(): void {
 		window.scroll(0, 0);
+		const interval = setInterval(() => {
+			if (this.seconds > 0) return --this.seconds;
+			clearInterval(interval);
+			this.router.navigateByUrl('/');
+		}, 1000);
 	}
 }
